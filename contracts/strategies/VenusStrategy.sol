e75ca05f79aeaf410bc0a7833af571103b9dc5ce
Eugene Elpashev <eelpashev@gmail.com>
2021-04-01 20:53:48 +0600

    Venus Strategy work.

--- contracts/strategies/VenusStrategy.sol
+++ contracts/strategies/VenusStrategy.sol
@@ -12,13 +12,19 @@ import "./IStrategy.sol";
 interface IUnitroller {
     function enterMarkets(address[] calldata vTokens) external returns (uint256[] memory);
 
-    function claimVenus(address account) external;
+    function claimVenus(address account, address[] calldata vTokens) external;
 }
 
 interface IVToken is IERC20Metadata {
     function mint(uint256 amount) external returns (uint256);
 
+    function redeem(uint256 amount) external returns (uint256);
+
+    function redeemUnderlying(uint256 amount) external returns (uint256);
+
     function underlying() external view returns (address);
+
+    function balanceOfUnderlying(address account) external view returns (uint256);
 }
 
 contract VenusStrategy is Ownable, IStrategy {
@@ -68,17 +74,22 @@ contract VenusStrategy is Ownable, IStrategy {
 
     function invest() external override onlyVault {
         uint256 balance = underlying.balanceOf(address(this));
+        // TODO: Move buffer to Vault
         if (balance > buffer) {
             vToken.mint(balance - buffer);
         }
     }
 
     function divest(uint256 amount) external override onlyVault {
-        unitroller.claimVenus(address(this));
+        address[] memory vTokens = new address[](1);
+        vTokens[0] = address(vToken);
+        unitroller.claimVenus(address(this), vTokens);
+
         uint256 balance = underlying.balanceOf(address(this));
         if (balance < amount) {
             uint256 missingAmount = amount - balance;
             require(missingAmount <= withdrawalCap, "VenusStrategy: reached withdrawal cap");
+            vToken.redeemUnderlying(missingAmount);
         }
         underlying.safeTransfer(address(vault), amount);
     }
@@ -91,14 +102,45 @@ contract VenusStrategy is Ownable, IStrategy {
         token.safeTransfer(to, amount);
     }
 
+    function depositUnderlying(uint256 amount) external onlyStrategist {
+        vToken.mint(amount);
+    }
+
+    function depositAll() external onlyStrategist {
+        vToken.mint(underlying.balanceOf(address(this)));
+    }
+
+    function withdrawShares(uint256 shares) external onlyStrategist {
+        vToken.redeem(shares);
+    }
+
+    function withdrawUnderlying(uint256 amount) public onlyStrategist {
+        vToken.redeemUnderlying(amount);
+    }
+
+    function withdrawAll() external onlyStrategist {
+        vToken.redeem(vToken.balanceOf(address(this)));
+    }
+
+    function setBuffer(uint256 _buffer) external onlyOwner {
+        buffer = _buffer;
+    }
+
+    function setWithdrawalCap(uint256 underlyingCap) external onlyOwner {
+        require(underlyingCap >= minWithdrawalCap, "VenusStrategy: withdrawal cap too low");
+        withdrawalCap = underlyingCap;
+    }
+
     function setStrategist(address _strategist) external onlyOwner {
         require(_strategist != address(0), "Strategy: strategist can't be zero address");
         strategist = _strategist;
     }
 
+    function calcTotalValue() external view override returns (uint256) {}
+
     function totalVenusDeposits() public view returns (uint256) {
         return vToken.balanceOf(address(this));
     }
 
-    function calcTotalValue() external view override returns (uint256) {}
+    function sharesForAmount(uint256 amount) internal view returns (uint256) {}
 }
